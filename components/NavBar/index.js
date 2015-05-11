61a283d80093b52c3d137d0283558d2435ef8098
Dustan Kasten <dustan.kasten@gmail.com>
2015-05-11 14:24:25 -0400

    Basic navbar toggling for mobile

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -7,13 +7,25 @@ import {Link} from 'react-router';
 var {PropTypes} = React;
 
 class NavBar extends React.Component {
+  constructor(props: Object, context: Object) {
+    super(props, context);
+
+    this.state = {showNav: true};
+    this.toggleNavigation = this.toggleNavigation.bind(this);
+  }
+
+  toggleNavigation(e: any) {
+    this.setState({showNav: !this.state.showNav});
+  }
+
   render(): ?ReactElement {
     return (
       <div className="NavBar">
         <img className="NavBar-logo" src="/public/images/logo.svg" />
-        <nav className="NavBar-nav">
+        <a onClick={this.toggleNavigation} className="NavBar-menu" href="#navigation" ariaLabel="Navigation">Menu</a>
+        <nav id="navigation" className={`NavBar-nav ${this.state.showNav ? 'is-visible' : ''}`}>
           {this.props.items.map(n => (
-            <Link className="NavBar-nav-link" to={n.pathName}>{n.text}</Link>
+            <Link className="NavBar-nav-link" key={n.pathName} to={n.pathName}>{n.text}</Link>
           ))}
         </nav>
       </div>
