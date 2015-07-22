94253fe58393bcfcf22c112857220a258ae39c23
Seth Paye <spaye@skookum.com>
2015-07-22 10:08:03 -0400

    update newsletter title

--- handlers/Contact/components/NewsletterInfo/index.js
+++ handlers/Contact/components/NewsletterInfo/index.js
@@ -10,7 +10,7 @@ class NewsletterInfo extends React.Component {
       <section className="NewsletterInfo">
         <header className="NewsletterInfo-header">
           <span className="NewsletterInfo-header-small">Subscribe to the</span>
-          <span>Skookum Files</span>
+          <span>Skookum Gazette</span>
         </header>
         <p className="NewsletterInfo-content">Where the bounds of literature are stretched, ideas are baked to a glorious temperature, and awesomeness is illustrated: the Skookum Gazette wants to reach your eye balls and crawl into your imagination. We want to build things with you, we want to share extrodinatary concepts, and we want to basically change the world.</p>
         <p className="NewsletterInfo-content NewsletterInfo-content--uppercase">Subscribe. Be bold.</p>
