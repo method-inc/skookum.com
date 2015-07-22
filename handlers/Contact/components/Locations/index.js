48188cf9591226085a961761d727b7f03e243ea9
Seth Paye <spaye@skookum.com>
2015-07-22 10:51:11 -0400

    make maps clickable and link to google maps

--- handlers/Contact/components/Locations/index.js
+++ handlers/Contact/components/Locations/index.js
@@ -9,7 +9,9 @@ import locations from 'locationsData';
 var renderSegment: ReactElement = (segment: Location) => (
   <TabPanel key={segment.name}>
     <div className="ContactLocations-panel">
-      <img className="ContactLocations-map" src={`https://maps.googleapis.com/maps/api/staticmap?center=${segment.addr + ' ' + segment.location}&markers=label:Skookum%7C${segment.addr}&zoom=12&size=400x400`} />
+      <a href={`http://maps.google.com/?q=${segment.addr + ' ' + segment.location}`} target="_blank">
+        <img className="ContactLocations-map" src={`https://maps.googleapis.com/maps/api/staticmap?center=${segment.addr + ' ' + segment.location}&markers=label:Skookum%7C${segment.addr}&zoom=12&size=400x400`} />
+      </a>
       <div className="ContactLocations-name">{`${segment.name} Office`}</div>
       <div className="ContactLocations-nickname">{segment.nickname}</div>
       <div className="ContactLocations-addr">{segment.addr}</div>
