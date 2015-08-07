bd9cf5d92043a0dd88cf1b1335cf671377fe337f
Seth Paye <spaye@skookum.com>
2015-08-07 16:11:55 -0400

    update prop types

--- components/Hamburger/index.js
+++ components/Hamburger/index.js
@@ -6,17 +6,16 @@ import {nameToHex} from 'nameToColor';
 class Hamburger extends Component {
   render(): ?ReactElement {
     var className = 'Hamburger';
-    var {target, onClick, color, overlayVisible} = this.props;
-    var hamburgerClass = this.props.color === 'yellow' ? ' is-black' : '';
+    var {target, onClick, overlayVisible} = this.props;
 
     if (overlayVisible) className = className + ' is-x';
 
     return (
       <a href={target} className={className} onClick={this.props.onClick}>
         <div className="Hamburger-icon">
-          <div className={'Hamburger-top' + hamburgerClass} />
-          <div className={'Hamburger-middle' + hamburgerClass} />
-          <div className={'Hamburger-bottom' + hamburgerClass} />
+          <div className={'Hamburger-top is-black'} />
+          <div className={'Hamburger-middle is-black'} />
+          <div className={'Hamburger-bottom is-black'} />
         </div>
       </a>
     );
@@ -24,6 +23,7 @@ class Hamburger extends Component {
 }
 
 Hamburger.propTypes = {
+  overlayVisible: PropTypes.boolean.isRequired,
   target: PropTypes.string.isRequired,
   onClick: PropTypes.func,
 };
