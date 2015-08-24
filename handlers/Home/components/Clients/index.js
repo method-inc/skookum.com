e9c936d3d2e05a932457b5c0c1cfed2cf946aaa7
Seth Paye <spaye@skookum.com>
2015-08-24 14:00:19 -0400

    update home page client to have description

--- handlers/Home/components/Clients/index.js
+++ handlers/Home/components/Clients/index.js
@@ -5,6 +5,7 @@ require('./styles.css');
 import React, {Component, PropTypes} from 'react';
 import {Resolver} from 'react-resolver';
 import api from 'api';
+import lookup from 'lookup';
 
 class Clients extends Component {
   render(): ?ReactElement {
@@ -22,7 +23,7 @@ class Clients extends Component {
           {this.props.clients.items.map(n => (
             <div className="HomeClients-client" key={n.name}>
               <div className="HomeClients-context">
-                Mobile streaming video backend for employee training
+                {lookup(n, 'description') || 'Nothing to see here, move along!'}
               </div>
               <div className="HomeClients-image-container">
                 <img className="HomeClients-img" title={n.name} src={n.image[0].fields.file.url} />
