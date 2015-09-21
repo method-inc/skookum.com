6bc2bf9439ff99dd342a7784d93eae691741d355
Seth Paye <spaye@skookum.com>
2015-09-21 16:38:09 -0400

    undo hosting videos locally, did not work

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -32,13 +32,14 @@ class Hero extends Component {
 
   renderBackground() {
 
-    var {videoName, image, poster} = this.props;
-    if (videoName && !this.state.isMobile) {
+    var {videos, image, poster} = this.props;
+    if (videos && videos.length > 0 && !this.state.isMobile) {
       return (
         <div>
           <video preload="auto" autoPlay muted loop className="Hero-video" poster={poster} >
-            <source src={`/public/videos/${videoName}.webm`} type="video/webm" />
-            <source src={`/public/videos/${videoName}.mp4`} type="video/mp4" />
+            {videos.map(video => (
+              <source key={video.fields.file.url} src={video.fields.file.url} type={video.fields.file.contentType} />
+            ))}
           </video>
           <div className="Hero-image Hero-image--mobile" style={{backgroundImage: `url(${image})`}} />
         </div>
