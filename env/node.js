c53247c52a0b49bba39944cf8195b6eb9c26c155
Seth Paye <spaye@skookum.com>
2015-11-06 13:00:54 -0500

    add the 4 new hardcoded redirect routes

--- env/node.js
+++ env/node.js
@@ -43,23 +43,27 @@ var baseRedirects = (req, res, next) => {
 };
 
 var REDIRECTS = [
-  ['case-studies', 'work'],
-  ['node', 'open-source'],
-  ['hunter-loftis-speaker-info', 'events'],
-  ['events/townhall-event-w-mayor-anthony-foxx', 'events'],
-  ['josh-oakhurst-speaker-info', 'events'],
-  ['events/mobile-apps-from-a-z-a-skookum-digital-workshop', 'events'],
-  ['visit-us', 'contact'],
-  ['living-and-working-in-charlotte-north-carolina-jobs-relocating', 'careers'],
-  ['jobs', 'careers'],
-  ['internet-of-things', 'the-internet-of-things-applications'],
-  ['it-consulting', 'capabilities'],
-  ['blog/tags/net', 'blog/tags/development'],
-  ['blog/node-js-you-learn-to-write-the-real-time-web/dsc_4121/', 'blog'],
-  ['blog/forget-native-learn-to-write-html5-apps/dsc_4048/', 'blog'],
-  ['blog/node-js-you-learn-to-write-the-real-time-web/dsc_4129/', 'blog'],
-  ['jobs', 'careers'],
-  ['resources/solve-for-bob.pdf', 'public/pdfs/solve-for-bob.pdf'],
+  ['case-studies', '/work'],
+  ['node', '/open-source'],
+  ['hunter-loftis-speaker-info', '/events'],
+  ['events/townhall-event-w-mayor-anthony-foxx', '/events'],
+  ['josh-oakhurst-speaker-info', '/events'],
+  ['events/mobile-apps-from-a-z-a-skookum-digital-workshop', '/events'],
+  ['visit-us', '/contact'],
+  ['living-and-working-in-charlotte-north-carolina-jobs-relocating', '/careers'],
+  ['jobs', '/careers'],
+  ['internet-of-things', '/the-internet-of-things-applications'],
+  ['it-consulting', '/capabilities'],
+  ['blog/tags/net', '/blog/tags/development'],
+  ['blog/node-js-you-learn-to-write-the-real-time-web/dsc_4121/', '/blog'],
+  ['blog/forget-native-learn-to-write-html5-apps/dsc_4048/', '/blog'],
+  ['blog/node-js-you-learn-to-write-the-real-time-web/dsc_4129/', '/blog'],
+  ['jobs', '/careers'],
+  ['resources/solve-for-bob.pdf', '/public/pdfs/solve-for-bob.pdf'],
+  ['press', '/contact'],
+  ['directions', '/contact'],
+  ['nightshift', 'http://www.nightshiftc.lt'],
+  ['why-charlotte', '/careers'],
 ];
 
 var tmpl = o => read('./index.html', 'utf8')
@@ -77,7 +81,7 @@ app.use('/email', express.static(join(process.cwd(), 'email')));
 REDIRECTS.forEach(function(redirect) {
   var [old, current] = redirect;
   app.get('/' + old, function(req, res) {
-    res.redirect(301, '/' + current);
+    res.redirect(301, current);
   });
 });
 
