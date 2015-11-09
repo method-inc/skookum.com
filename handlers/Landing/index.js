8697b2f5087ebbfb96f394918d3148fd52b0ba9b
Seth Paye <spaye@skookum.com>
2015-11-09 13:23:03 -0500

    add clients and fixed width

--- handlers/Landing/index.js
+++ handlers/Landing/index.js
@@ -1,8 +1,10 @@
 require('./styles.css');
 
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import {Resolver} from 'react-resolver';
 import Hero from 'Hero';
+import api from 'api';
+import lookup from 'lookup';
 
 class Landing extends React.Component {
 
@@ -11,7 +13,7 @@ class Landing extends React.Component {
   render(): ?ReactElement {
     return (
       <div className="Landing">
-        <section className="Landing-container Landing-container--fixed">
+        <section className="Landing-container is-fixed">
           <div className="Landing-description">
             Whether you’re looking to build your own cloud, or leverage existing providers like AWS and Azure, we can customize a cloud strategy to meet your specific business needs. From strategic planning to implementation and support, we offer a complete range of services to help complement in-house capabilities and accelerage cloud adoption. 
           </div>
@@ -25,7 +27,7 @@ class Landing extends React.Component {
 
         <section className="Landing-container">
           <div className="Landing-layer">
-            <ul className="Landing-stats">
+            <ul className="Landing-stats is-left">
               <li className="Landing-stat">
                 <div className="Landing-stat-number">10</div>
                 <div className="Landing-stat-text">years in business</div>
@@ -39,32 +41,45 @@ class Landing extends React.Component {
                 <div className="Landing-stat-text">years in business</div>
               </li>
             </ul>
-            <div className="Landing-layer-content">
+            <div className="Landing-layer-container">
+              <div className="Landing-layer-image" style={{backgroundImage: `url(../../public/images/blogimg_all.png)`}}/>
             </div>
           </div>
           <div className="Landing-layer">
-            <div className="Landing-layer-content">
-              <div className="Landing-layer-title">
-                Why Skookum
-              </div>
-              <div className="Landing-layer-description">
-                The age of the customer is changing everything about the way companies go to market. As a long time partner for progressive CIOs and IT leaders, we have successfully delivered custom cloud solutions to help companies evolve in the new digital economy.
+            <div className="Landing-layer-container">
+              <div className="Landing-layer-content">
+                <div className="Landing-layer-title">
+                  Why Skookum
+                </div>
+                <div className="Landing-layer-description">
+                  The age of the customer is changing everything about the way companies go to market. As a long time partner for progressive CIOs and IT leaders, we have successfully delivered custom cloud solutions to help companies evolve in the new digital economy.
+                </div>
               </div>
             </div>
-            <ul className="Landing-stats">
-              <li className="Landing-stat">
+            <ul className="Landing-stats is-right">
+              <li className="Landing-stat is-right">
                 <div className="Landing-stat-number">10</div>
                 <div className="Landing-stat-text">years in business</div>
               </li>
-              <li className="Landing-stat">
+              <li className="Landing-stat is-right">
                 <div className="Landing-stat-number">10</div>
                 <div className="Landing-stat-text">years in business</div>
               </li>
-              <li className="Landing-stat">
+              <li className="Landing-stat is-right">
                 <div className="Landing-stat-number">10</div>
                 <div className="Landing-stat-text">years in business</div>
               </li>
             </ul>
+          </div>
+        </section>
+
+        <section className="Landing-container is-fixed">
+          <div className="Landing-clients">
+            {this.props.clients.items.map(n => (
+              <div className="Landing-client" key={n.name}>
+                  <img className="Landing-client-image" title={n.name} src={n.image[0].fields.file.url} alt={n.image[0].fields.title} />
+              </div>
+            ))}
           </div>
         </section>
       </div>
@@ -73,17 +88,15 @@ class Landing extends React.Component {
 }
 
 Landing.propTypes = {
-  // promise: React.PropTypes.string.isRequired,
+  clients: PropTypes.any.isRequired,
 };
 
 Landing.displayName = 'Landing';
 
 export default Resolver.createContainer(Landing, {
   resolve: {
-    /*
-    promise() {
-      return PromiseStore.find(this.getParams().id);
-    }
-    */
+    clients() {
+      return api(`contentful?content_type=client&limit=7`);
+    },
   },
 });
