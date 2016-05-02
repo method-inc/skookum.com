05cc200ba48518f0f12473b9e2afaddf125afa2d
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2016-05-02 15:36:25 -0300

    removed if

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -43,24 +43,6 @@ class Footer extends React.Component {
 
     const getYear = new Date().getFullYear();
 
-    if (this.context.router.getCurrentPathname() === '/contact'){
-      return (
-        <div>
-          <footer className="Footer">
-            <div className="Footer-links">
-              <Link to="careers" className="Footer-link">Careers</Link>
-              <Link to="open-source" className="Footer-link">Open&nbsp;Source</Link>
-              <a href="https://www.twitter.com/skookum" target="_blank" className="Footer-link">Twitter</a>
-              <a href="https://www.linkedin.com/company/skookum-digital-works" target="_blank" className="Footer-link">Linkedin</a>
-              <a href="https://www.facebook.com/SkookumInc" target="_blank" className="Footer-link">Facebook</a>
-              <a href="https://instagram.com/skookumpeople" target="_blank" className="Footer-link">Instagram</a>
-            </div>
-            <p className="Footer-legal">Ⓒ {getYear} Skookum. All Rights Reserved.</p>
-          </footer>
-        </div>
-      );
-    }
-
     return (
       <div>
         {this.context.router.getCurrentPathname() === '/contact' ? '' :
