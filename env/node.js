a4612fe1c1c06a8be67ebe3c618ee45038d8e3f0
Jacob LeGrone <git@jacoblegrone.com>
2016-09-15 10:38:57 -0400

    Add staging subdomain

--- env/node.js
+++ env/node.js
@@ -17,7 +17,7 @@ import fs from 'fs';
 
 var siteMap = fs.readFileSync(join(process.cwd(), 'sitemap.xml'));
 
-var validPaths = ['blog', 'capabilities', 'work', 'contact', 'events', 'open-source', 'innovation-camp', 'careers'];
+var validPaths = ['blog', 'capabilities', 'work', 'contact', 'events', 'open-source', 'innovation-camp', 'careers', 'staging'];
 
 var baseRedirects = (req, res, next) => {
   var redirect = false;
